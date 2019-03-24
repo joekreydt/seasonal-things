28e00ffe83a1501cc14390ebac76037111e00e46
Glitch (candle-celsius) <none>
2019-03-24 00:37:05 +0000

    🍔💾 Checkpoint ./script.js:2847504/392 ./index.html:2847504/96

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function generateList() {
   console.log("Button clicked!")
+  
+  
+  if (document.getElementById("winterCheckbox").checked == true) {
+    console.log("Winter is checked.")
+      var mydata = JSON.parse("media.json");
+      alert(mydata[0].name);
+      alert(mydata[0].age);
+      alert(mydata[1].name);
+      alert(mydata[1].age);
+  }
 }
