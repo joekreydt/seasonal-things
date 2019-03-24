a7cc0de5efa901d06ccada711e7e55263087ae79
Glitch (candle-celsius) <none>
2019-03-24 00:48:05 +0000

    🔦🎫 Checkpoint ./script.js:2847504/15898 ./index.html:2847504/1

--- script.js
+++ script.js
@@ -1,13 +1,559 @@
+var media = `{
+  "season" : [
+    {
+      "winter" : [
+        {
+          "mediaType" : [
+            {
+              "book" : [
+                {
+                  "title" : "Misery",
+                  "author" : "Stephen King"
+                },
+                {
+                  "title" : "True Grit",
+                  "author" : "Charles Portis"
+                },
+                {
+                  "title" : "The Bear and the Nightingale",
+                  "author" : "Katherine Arden"
+                },
+                {
+                  "title" : "Catcher in the Rye",
+                  "author" : "JD Salinger"
+                },
+                {
+                  "title" : "Snow Child",
+                  "author" : "Eowyn Ivey"
+                },
+                {
+                  "title" : "To the Bright Edge of the World",
+                  "author" : "Eowyn Ivey"
+                },
+                {
+                  "title" : "Brian's Winter",
+                  "author" : "Gary Paulsen"
+                },
+                {
+                  "title" : "Snow Country",
+                  "author" : "Yasunari Kawabata"
+                }
+              ]
+            },
+            {
+              "film" : [
+                {
+                  "title" : "Reindeer Games"
+                },
+                {
+                  "title" : "The Bourne Identity"
+                },
+                {
+                  "title" : "Zach and Miri"
+                },
+                {
+                  "title" : "The Hateful Eight"
+                },
+                {
+                  "title" : "It Happened on Fifth Avenue"
+                },
+                {
+                  "title" : "Christmas Vacation"
+                },
+                {
+                  "title" : "On Her Majesty's Secret Service (James Bond)"
+                },
+                {
+                  "title" : "Trading Places"
+                },
+                {
+                  "title" : "Mr Deeds"
+                },
+                {
+                  "title" : "Dumb and Dumber"
+                },
+                {
+                  "title" : "Balto"
+                },
+                {
+                  "title" : "The Mighty Ducks"
+                },
+                {
+                  "title" : "101 Dalmations"
+                },
+                {
+                  "title" : "Surf's Up"
+                },
+                {
+                  "title" : "Happy Feet"
+                },
+                {
+                  "title" : "Out Cold"
+                },
+                {
+                  "title" : "Fargo"
+                },
+                {
+                  "title" : "The Chronicles of Narnia: The Lion, the Witch, and the Wardrobe"
+                },
+                {
+                  "title" : "Spectre (James Bond)"
+                },
+                {
+                  "title" : "Goldeneye (James Bond)"
+                },
+                {
+                  "title" : "Harry Potter (series)"
+                },
+                {
+                  "title" : "Lars and the Real Girl"
+                },
+                {
+                  "title" : "A Case of You"
+                },
+                {
+                  "title" : "The Hobbit (series)"
+                },
+                {
+                  "title" : "Lord of the Rings (series)"
+                },
+                {
+                  "title" : "Higher"
+                },
+                {
+                  "title" : "Deeper"
+                },
+                {
+                  "title" : "Further"
+                },
+                {
+                  "title" : "Eternal Sunshine of the Spotless Mind"
+                },
+                {
+                  "title" : "Beautiful Girls"
+                },
+                {
+                  "title" : "Journey to Greenland"
+                },
+                {
+                  "title" : "Under the Tuscan Sun"
+                },
+                {
+                  "title" : "Just Friends"
+                },
+                {
+                  "title" : "The Proposal"
+                },
+                {
+                  "title" : "The Shop Around the Corner"
+                },
+                {
+                  "title" : "12 Dates of Christmas"
+                },
+                {
+                  "title" : "Serendipity"
+                },
+                {
+                  "title" : "Rocky"
+                },
+                {
+                  "title" : "In Bruges"
+                },
+                {
+                  "title" : "Uncle Buck"
+                },
+                {
+                  "title" : "Scott Pilgrim vs the World"
+                },
+                {
+                  "title" : "Nick and Norah's Infinite Playlist"
+                },
+                {
+                  "title" : "Ice Age"
+                },
+                {
+                  "title" : "The Fountain"
+                },
+                {
+                  "title" : "Jeremiah Johnson"
+                },
+                {
+                  "title" : "The Fifth Estate"
+                },
+                {
+                  "title" : "South Park: Bigger, Longer & Uncut"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "spring" : [
+        {
+          "mediaType" : [
+            {
+              "book" : [
+                {
+                  "title" : "1984",
+                  "author" : "George Orwell"
+                },
+                {
+                  "title" : "The Goldfinch",
+                  "author" : "Donna Tartt"
+                },
+                {
+                  "title" : "The Perks of Being a Wallflower",
+                  "author" : "Stephen Chbosky"
+                },
+                {
+                  "title" : "When We Were Saints",
+                  "author" : "Han Nolan"
+                },
+                {
+                  "title" : "The Life Changing Magic of Tidying Up",
+                  "author" : "Marie Kondo"
+                }
+              ]
+            },
+            {
+              "film" : [
+                {
+                  "title" : "40 Days and 40 Nights"
+                },
+                {
+                  "title" : "The Boondock Saints"
+                },
+                {
+                  "title" : "Wedding Crashers"
+                },
+                {
+                  "title" : "Nick and Norah's Infinite Playlist"
+                },
+                {
+                  "title" : "Blue Valentine"
+                },
+                {
+                  "title" : "Valentine's Day"
+                },
+                {
+                  "title" : "Garden State"
+                },
+                {
+                  "title" : "Under the Tuscan Sun"
+                },
+                {
+                  "title" : "Midnight in Paris"
+                },
+                {
+                  "title" : "Napoleon Dynamite"
+                },
+                {
+                  "title" : "Paterson"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "summer" : [
+        {
+          "mediaType" : [
+            {
+              "book" : [
+                {
+                  "title" : "The Sun Also Rises",
+                  "author" : "Ernest Hemingway"
+                },
+                {
+                  "title" : "A Salty Piece of Land",
+                  "author" : "Jimmy Buffett"
+                }
+              ]
+            },
+            {
+              "film" : [
+                {
+                  "title" : "Love & Mercy"
+                },
+                {
+                  "title" : "Wedding Crashers"
+                },
+                {
+                  "title" : "Road House"
+                },
+                {
+                  "title" : "Mr Hobbs Takes a Vacation"
+                },
+                {
+                  "title" : "Flipper"
+                },
+                {
+                  "title" : "Fast & Furious (series)"
+                },
+                {
+                  "title" : "National Lampoon's Vacation"
+                },
+                {
+                  "title" : "Starsky & Hutch"
+                },
+                {
+                  "title" : "The Endless Summer"
+                },
+                {
+                  "title" : "Into the Blue"
+                },
+                {
+                  "title" : "Fool's Gold"
+                },
+                {
+                  "title" : "Rocket Power: Island of the Menehune"
+                },
+                {
+                  "title" : "The Wackness"
+                },
+                {
+                  "title" : "Jaws"
+                },
+                {
+                  "title" : "Begin Again"
+                },
+                {
+                  "title" : "The Hobbit: An Unexpected Journey"
+                },
+                {
+                  "title" : "A Good Year"
+                },
+                {
+                  "title" : "Moonrise Kingdom"
+                },
+                {
+                  "title" : "Papa Hemingway in Cuba"
+                },
+                {
+                  "title" : "Dragon"
+                },
+                {
+                  "title" : "The Big Boss"
+                },
+                {
+                  "title" : "The Life Aquatic with Steve Zissou"
+                },
+                {
+                  "title" : "Under the Tuscan Sun"
+                },
+                {
+                  "title" : "The Breakup"
+                },
+                {
+                  "title" : "Masterminds"
+                },
+                {
+                  "title" : "Chef"
+                },
+                {
+                  "title" : "Couples Retreat"
+                },
+                {
+                  "title" : "Waffle Street"
+                },
+                {
+                  "title" : "Paterson"
+                },
+                {
+                  "title" : "Eat Drink Man Woman"
+                },
+                {
+                  "title" : "Dirty Dancing"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "autumn" : [
+        {
+          "mediaType" : [
+            {
+              "book" : [
+                {
+                  "title": "Dracula",
+                  "author": "Bram Stoker"
+                },
+                {
+                  "title" : "The Witches of Eastwick",
+                  "author" : "John Updike"
+                },
+                {
+                  "title" : "Staggerford",
+                  "author" : "Jon Hassler"
+                },
+                {
+                  "title" : "Dead Poets Society",
+                  "author" : "Nancy H Kleinbaum"
+                },
+                {
+                  "title" : "The October Country",
+                  "author" : "Ray Bradbury"
+                },
+                {
+                  "title" : "The Halloween Tree",
+                  "author" : "Ray Bradbury"
+                },
+                {
+                  "title" : "Something Wicked This Way Comes",
+                  "author" : "Ray Bradbury"
+                },
+                {
+                  "title" : "Silver Linings Playbook",
+                  "author" : "Matthew Quick"
+                },
+                {
+                  "title" : "It",
+                  "author" : "Stephen King"
+                },
+                {
+                  "title" : "Jane Eyre",
+                  "author" : "Charlotte Brontë"
+                },
+                {
+                  "title" : "The Gustav Sonata",
+                  "author" : "Rose Tremain"
+                },
+                {
+                  "title" : "Fahrenheit 451",
+                  "author" : "Ray Bradbury"
+                },
+                {
+                  "title" : "The Perks of Being a Wallflower",
+                  "author" : "Stephen Chbosky"
+                },
+                {
+                  "title" : "Snow Country",
+                  "author" : "Yasunari Kawabata"
+                }
+              ]
+            },
+            {
+              "movie" : [
+                {
+                  "title" : "The Karate Kid"
+                },
+                {
+                  "title" : "Dan in Real Life"
+                },
+                {
+                  "title" : "Road Trip"
+                },
+                {
+                  "title" : "Road House"
+                },
+                {
+                  "title" : "Big Daddy"
+                },
+                {
+                  "title" : "The Haunted Mansion"
+                },
+                {
+                  "title" : "Zombieland"
+                },
+                {
+                  "title" : "Ghostbusters"
+                },
+                {
+                  "title" : "Wallace & Gromit: The Curse of the Were-Rabbit"
+                },
+                {
+                  "title" : "Paranorman"
+                },
+                {
+                  "title" : "Hotel Transylvania"
+                },
+                {
+                  "title" : "Donnie Darko"
+                },
+                {
+                  "title" : "The Nightmare Before Christmas"
+                },
+                {
+                  "title" : "Goosebumps"
+                },
+                {
+                  "title" : "Casper"
+                },
+                {
+                  "title" : "Liberal Arts"
+                },
+                {
+                  "title" : "Stuck in Love"
+                },
+                {
+                  "title" : "Harry Potter (series)"
+                },
+                {
+                  "title" : "The Secret Life of Pets"
+                },
+                {
+                  "title" : "Paterson"
+                },
+                {
+                  "title" : "Dirty Dancing"
+                },
+                {
+                  "title" : "Planes, Trains & Automobiles"
+                },
+                {
+                  "title" : "It"
+                },
+                {
+                  "title" : "Moonrise Kingdom"
+                },
+                {
+                  "title" : "Nick and Norah's Infinite Playlist"
+                },
+                {
+                  "title" : "10 Cloverfield Lane"
+                },
+                {
+                  "title" : "The Proposal"
+                },
+                {
+                  "title" : "When Harry Met Sally"
+                },
+                {
+                  "title" : "Pocahontas"
+                },
+                {
+                  "title" : "The New Guy"
+                },
+                {
+                  "title" : "The Edge of Seventeen"
+                },
+                {
+                  "title" : "The Witches of Eastwick"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    } 
+  ]  
+}`
+
+
 function generateList() {
-  console.log("Button clicked!")
+  console.log("Button clicked!");
   
   
   if (document.getElementById("winterCheckbox").checked == true) {
-    console.log("Winter is checked.")
-      var mydata = JSON.parse("media.json");
-      alert(mydata[0].name);
-      alert(mydata[0].age);
-      alert(mydata[1].name);
-      alert(mydata[1].age);
-  }
-}
+    console.log("Winter is checked.");
+      var mediaParse = JSON.parse(media);
+      console.log(media.season);
+  };
+};
